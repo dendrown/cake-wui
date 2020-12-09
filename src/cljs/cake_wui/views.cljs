e73346847f25a7257f7672ac0bc92e1951904362
den Drown <den@dendrown.net>
2020-12-08 23:52:32 -0500

    One foot forward

--- src/cljs/cake_wui/views.cljs
+++ src/cljs/cake_wui/views.cljs
@@ -15,9 +15,15 @@
    [re-frame.core :as rf]
    [cake-wui.subs :as subs]))
 
+(defn footer []
+  (let [now (js/Date.)]
+    [:div.footer
+     [:p "Copyright ©" (.getFullYear now)]]))
+
 
 ;;; --------------------------------------------------------------------------
 (defn main-panel []
   (let [name (rf/subscribe [::subs/name])]
     [:div
-     [:h1 "Let's eat " @name]]))
+     [:h1 "Let's eat " @name]
+     [footer]]))
